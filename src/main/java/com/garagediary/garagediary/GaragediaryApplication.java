2ae3f5e8f1c102f263b7c396267ac5e0b660a9d2
v38225744-hue <v38225744@gmail.com>
2025-11-17 13:11:25 +0530

    initial commit

---
+++ src/main/java/com/garagediary/garagediary/GaragediaryApplication.java
@@ -0,0 +1,13 @@
+package com.garagediary.garagediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragediaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragediaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garagediary/garagediary/GaragediaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garagediary.garagediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragediaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
